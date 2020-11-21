211807bfaedd7d7871e85594ea17616cdf27be73
Eduardo Simón <edu@arkerlabs.com>
2020-11-21 12:28:25 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# My personal DOTFILES 🛠
+My custom dotfiles using the framework dotly
+
+# Prerequisites
+Mac or Linux
+
+
+# Installing
+## Clone the repo
+```sh
+git clone --recurse-submodules https://github.com/EduardoSimon/dotfiles.git .dotfiles 
+````
+
+## Install the framework
+
+```sh
+dot self install
+````
+
+Dont forget to restart the terminal! 🤓
+
---
+++ shell/init.sh
@@ -0,0 +1,9 @@
+# This is a useful file to have the same aliases/functions in bash and zsh
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Register custom aliases and functions
+for aliasToSource in "$DOTFILES_PATH/shell/_aliases/"*; do source "$aliasToSource"; done
+for exportToSource in "$DOTFILES_PATH/shell/_exports/"*; do source "$exportToSource"; done
+for functionToSource in "$DOTFILES_PATH/shell/_functions/"*; do source "$functionToSource"; done
