2ce388fe2124aa1e9660ce7351d26f0ad958a71e
Eduardo Simón <esimon@eventscase.com>
2020-12-01 18:06:10 +0100

    add check-merged-branches scripts

--- shell/_aliases/ec.sh
+++ shell/_aliases/ec.sh
@@ -1,2 +1,4 @@
 alias styles="echo \"Ejecutando less_compile\" && curl -s \"http://eventscase.loc/tools/less_compile\""
+alias regendb="echo \"Ejecutando regenerate_db\" && curl -s \"http://eventscase
+.loc/tools/regenerate_db\""
 alias dbch="dot ec change-db"
