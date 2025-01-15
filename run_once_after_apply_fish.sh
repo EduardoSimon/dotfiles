837d24006741e8e79ba4c4c53c549dec85fec9a8
Eduardo Simon <edusimonpicon@gmail.com>
2025-01-15 09:15:56 +0100

    add fish plugins install script

---
+++ run_once_after_apply_fish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install PatrickF1/fzf.fish
+fisher install gazorby/fish-abbreviation-tips
+fisher install jethrokuan/z
+fisher install jhillyerd/plugin-git
