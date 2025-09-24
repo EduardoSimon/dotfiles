1a6e591d2e0caaddbee1ae20dfefb340cea90016
Eduardo Simon <edusimonpicon@gmail.com>
2025-09-25 00:35:18 +0200

    use correct fish path on install

--- run_once_after_apply_1_fish.sh
+++ run_once_after_apply_1_fish.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env fish
+#!/usr/bin/env /opt/homebrew/bin/fish
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
