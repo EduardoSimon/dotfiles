b5b5ac4188c57ab2edf34974b4d4b9d526cc05e1
Eduardo Simon <edusimonpicon@gmail.com>
2025-09-24 18:08:31 +0200

    do not ask for password in mac after sleep

--- executable_mac-os.sh
+++ executable_mac-os.sh
@@ -78,10 +78,6 @@ sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bo
 # Screen                                                                      #
 ###############################################################################
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
